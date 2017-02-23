af65af2c116ee6288d275c11badc41b9d557a1cb
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2017-02-23 12:36:12 +0530

    remove concurrency for copy

--- lib/fast_io_with_postgrex.ex
+++ lib/fast_io_with_postgrex.ex
@@ -3,28 +3,18 @@ defmodule FastIoWithPostgrex do
 
     {:ok, pid} = Postgrex.start_link(name: :pg, pool: DBConnection.Poolboy, pool_size: 8, hostname: "localhost", username: "postgres", password: "postgres", database: "fp")
 
+    Postgrex.transaction(:pg, fn conn ->
+      pg_copy = Postgrex.stream(conn, "COPY words(id, word) FROM STDIN", [])
 
-    File.stream!(filepath)
-    |> Stream.map(fn line ->
-      [id_str, word] = line |> String.trim |> String.split("\t", trim: true, parts: 2)
 
-      {id, ""} = Integer.parse(id_str)
+      File.stream!(filepath)
+      |> Enum.map(fn line ->
+        [id, word] = line |> String.trim |> String.split("\t", trim: true, parts: 2)
+        [id, ?\t, word, ?\n]
+      end)
+      |> Enum.into(pg_copy)
 
-      [id, word]
-    end)
-    |> Stream.chunk(10_000, 10_000, [])
-    |> Task.async_stream(fn word_rows ->
-      Postgrex.transaction(:pg, fn conn ->
-        pg_copy = Postgrex.stream(conn, "COPY words(id, word) FROM STDIN", [])
-
-        word_rows # => [[1, "Awesome"], [2, "Raw"]]
-        |> Enum.map(fn [id, word] ->
-          [to_string(id), ?\t, word, ?\n] end)
-        |> Enum.into(pg_copy)
-
-      end, pool: DBConnection.Poolboy, pool_timeout: :infinity, timeout: :infinity)
-    end, max_concurrency: 8, timeout: :infinity)
-    |> Stream.run
+    end, pool: DBConnection.Poolboy, pool_timeout: :infinity, timeout: :infinity)
   end
 
 end
